955a096e3f5cca1c63580d76f3eafe287d00e793
MS_user <theatra.quirrel@gmail.com>
2024-11-11 18:00:12 +0200

    Homework_3

--- pythonProject/hw_14.1.py
+++ pythonProject/hw_14.1.py
@@ -1,12 +1,15 @@
-class Human:
+from logging import raiseExceptions
 
+
+class Human:
+    # Ініціюємо клас Human:
     def __init__(self, gender, age, first_name, last_name):
         self.gender = gender
         self.age = age
         self.first_name = first_name
         self.last_name = last_name
 
-    # Прописуємо виведення інформації про Human:
+    # Прописуємо виведення інформації про його екземпляр:
     def __str__(self):
         return f"{self.first_name} {self.last_name} is a {self.age}yo {self.gender}."
 
@@ -18,21 +21,36 @@ class Student(Human):
 
     def __str__(self):
         return f"Student {self.first_name} {self.last_name} is a {self.age}yo {self.gender} with a record book {self.record_book}."
+
+
+
+
+# NEW # Створюємо власний клас виключення (для випадків перевищення максимуму в 10 студентів у групі):
+class ExceededNumberError(Exception):
+        pass
+
+
+
+
 # Описуємо параметри нового класу, створюючи порожню множину для зберігання списку студентів:
 class Group:
     def __init__(self, number):
         self.number = number
         self.group = set()
-    # Створюємо функції додавання, пошуку і видалення (на базі пошуку):
+
+
+
+
+    # NEW # Створюємо функції додавання, пошуку і видалення (на базі пошуку); прописуємо викидання виклику:
     def add_student(self, student):
+        if len(self.group) >= 10:
+                print("Max. number of students is 10!")
+                raise ExceededNumberError("Max. number of students exceeded!")
+
         self.group.add(student)
 
-    def delete_student(self, last_name):
-        student = self.find_student(last_name)
-        if student:
-            self.group.remove(student)
-        else:
-            print(f"No student with last name '{last_name}' found.")
+
+
 
     def find_student(self, last_name):
         for student in self.group:
@@ -40,6 +58,13 @@ class Group:
                 print(f"Student {last_name} studies in a group {self.number}.")
                 return student
         return None
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+        else:
+            print(f"No student with last name '{last_name}' found.")
 
     # Для рядкового представлення Групи виводимо номер і елементи множини з нового рядка:
     def __str__(self):
@@ -57,10 +82,35 @@ print(student)
 
 st1 = Student('male', 30, 'Steve', 'Jobs', 'AN142')
 st2 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+st3 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+st4 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+st5 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+st6 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+st7 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+st8 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+st9 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+st10 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+st11 = Student('female', 25, 'Liza', 'Taylor', 'AN145')
+
+
 
 gr = Group('PD1')
-gr.add_student(st1)
-gr.add_student(st2)
+
+students_to_add = [st1, st2, st3, st4, st5, st6, st7, st8, st9, st10, st11]
+
+
+
+
+# NEW # Створюємо блок, що оброблятиме виняток в методі add_student:
+for student in students_to_add:
+    try:
+        gr.add_student(student)
+    except ExceededNumberError as e:
+        print(f"Error: {e}")
+
+
+
+
 print(gr)
 
 assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
@@ -71,3 +121,4 @@ gr.delete_student('Taylor')
 print(gr)  # Only one student
 
 gr.delete_student('Taylor')  # No error!
+print("Ok")
